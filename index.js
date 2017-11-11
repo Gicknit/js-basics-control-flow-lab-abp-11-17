63f445e826dfc0da0676177e598b6c869869df08
Nick Garcia <89nickgarcia@gmail.com>
2017-11-11 23:18:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,9 @@ function switchOnCharmFromTip(tip){
   switch (tip){
     case 'generous':
     return ("Thank you so much.")
+    break;
+    case 'not as generous':
+    return ("Thank you")
+    break;
   }
 }
